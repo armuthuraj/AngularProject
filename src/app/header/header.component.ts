a88ae3802bc3f9c91f2adc62f4f67466ffee9871
Muthu <armuthuraj@gmail.com>
2023-04-09 21:12:27 -0600

    push 3

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  heading: string = 'One stop shop for everything';
 
 }
--- src/app/notification/notification.component.ts
+++ src/app/notification/notification.component.ts
@@ -4,10 +4,14 @@ import { Component } from '@angular/core';
   selector: 'app-notification',
   template: `<div>
                 <p>This Website uses cookies to provide better user experience.</p>
+                <div class="close"><button class="btn" (click)="closeNotification()")>X</button></div>
               </div>`,
-  styles: ["div{margin: 10px 0px; padding: 20px 10px; background-color:#FAD7A0; text-align: center;}","p{font-size: 14px;}"]
+  styles: ["div{margin: 10px 0px; padding: 20px 10px; background-color:#FAD7A0; text-align: center;}","p{font-size: 14px;}",".close{float: right; margin-top: -39px;}"]
 })
 export class NotificationComponent {
+  closeNotification(){
+    
+  }
 
 }
-  
+   
